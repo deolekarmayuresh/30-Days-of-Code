dd471ea2b2e4d4528598285e1e8abf25596d65c3
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-19 12:50:00 +0530

    day 3-solution

--- Day 3 Intro to Conditional Statements/day3-solution.js
+++ Day 3 Intro to Conditional Statements/day3-solution.js
@@ -1,2 +1,25 @@
 // code snippet for Day 3: Intro to Conditional Statements
 
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const N = parseInt(readLine(), 10);
+
+    if((N%2)==0){
+        if((N>=2 && N<=5) || N>20)
+        {
+            console.log("Not Weird");
+        }
+        else
+        {
+            console.log("Weird");
+        }
+    }
+    else
+    {
+        console.log("Weird"); 
+    }
+}
+
