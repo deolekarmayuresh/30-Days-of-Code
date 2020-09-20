841be04840f4fa8d36acbe49fdf868b476960b84
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-20 16:28:13 +0530

    day 4 solution

--- Day 4 Class vs. Instance/day4-solution.js
+++ Day 4 Class vs. Instance/day4-solution.js
@@ -1,1 +1,42 @@
 // code snippet for Day 4 Classes & Instances
+
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+     if(initialAge >= 0) {
+         let age = initialAge;
+        
+       }else{
+            initialAge = 0;
+            console.log("Age is not valid, setting age to 0.");
+       }
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+    if(initialAge < 13) {
+         console.log("You are young.");
+      }else if(initialAge >= 13 && initialAge < 18) {
+          console.log("You are a teenager.");
+      }else {
+          console.log("You are old.");
+      }
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+          initialAge = initialAge+1;
+   };
+}
+
+function main() {
+
+var T=parseInt(readLine());
+for(i=0;i<T;i++){
+    var age=parseInt(readLine());
+    var p=new Person(age);
+    p.amIOld();
+    for(j=0;j<3;j++){
+        p.yearPasses();
+        
+    }
+    p.amIOld();
+    console.log("");   
+    }
+}
