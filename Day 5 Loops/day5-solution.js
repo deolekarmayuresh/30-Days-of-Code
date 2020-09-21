eda4f71b4c943c9571eef7234a37c4bda4330a57
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-21 13:22:22 +0530

    day 5 solution

--- Day 5 Loops/day5-solution.js
+++ Day 5 Loops/day5-solution.js
@@ -1,2 +1,12 @@
 // code snippet for Day 5: Loops
 
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    for(let i=1;i<11;i++){
+        console.log(`${n} x ${i} = ${n * i}`)
+    }
+}
