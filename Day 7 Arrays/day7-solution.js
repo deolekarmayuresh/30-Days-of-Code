b90dde3882cbfd3d2aceaf9b5fac49d92694ad94
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-23 16:07:28 +0530

    day 7 solution

--- Day 7 Arrays/day7-solution.js
+++ Day 7 Arrays/day7-solution.js
@@ -1,2 +1,12 @@
 //code snippet for Day 7: Arrays
 
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    console.log(arr.reverse().toString().replace(/,/g,' '));
+}
