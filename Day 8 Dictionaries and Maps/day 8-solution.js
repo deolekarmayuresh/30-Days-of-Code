12c8ecbe486a9621c0d1e7884e96b81187589b6a
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-24 23:54:29 +0530

    day 8 solution

--- Day 8 Dictionaries and Maps/day 8-solution.js
+++ Day 8 Dictionaries and Maps/day 8-solution.js
@@ -1,2 +1,34 @@
 // code snippet for Day 8: Dictionaries and Maps
 
+function processData(input) {
+    //Enter your code here
+    let tempArry = input.split("\n");
+    let entriesLen = tempArry.splice(0, 1); //get the number of entries out of the array
+    let queries = tempArry.splice(entriesLen); //dump the queries into their own array
+
+    for (let i = 0; i < entriesLen; i++) {
+    tempArry[i] = tempArry[i].split(" ");//split the names from the phones to prepare for mapping
+  }
+  
+  //turn the array into a map
+  var phonebk = new Map(tempArry);
+    queries.map(name => {
+    if(phonebk.has(name)){
+        console.log(name + "=" + phonebk.get(name));
+    }else{
+        console.log("Not found");
+    }
+})
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
