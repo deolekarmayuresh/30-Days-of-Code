f52df75ae9291f80bed5bf5fb0c7a25f16a3599b
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-26 22:10:41 +0530

    day 10 solution

--- Day 10 Binary Numbers/day 10-solution.js
+++ Day 10 Binary Numbers/day 10-solution.js
@@ -1,2 +1,30 @@
 // code snippet for Day 10: Binary Numbers
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    console.log(n.toString(2).split('0').sort((a, b) => a < b)[0].length);
+}
