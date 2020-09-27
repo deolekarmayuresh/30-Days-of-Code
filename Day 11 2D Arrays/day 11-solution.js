095a230327374a840d5884997c5ce3dcb4d04ab3
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-09-27 20:38:13 +0530

    day 11 solution

--- Day 11 2D Arrays/day 11-solution.js
+++ Day 11 2D Arrays/day 11-solution.js
@@ -1,2 +1,24 @@
 // code snippet for Day 11: 2D Arrays
 
+function main() {
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    let total = -63;
+    for (let i=0; i < arr.length-2; i++) {
+        for (let j=0, curSum = 0; j < arr[i].length-2; j++) {
+            curSum = arr[i][j] + 
+                     arr[i][j+1] + 
+                     arr[i][j+2] + 
+                     arr[i+1][j+1] + 
+                     arr[i+2][j] + 
+                     arr[i+2][j+1] + 
+                     arr[i+2][j+2];
+            if (curSum > total) total = curSum;
+        }
+    }
+    console.log(total);
+}
