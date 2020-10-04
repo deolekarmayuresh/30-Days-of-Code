0c67ff463e4c40e2e128837ab0264b84b9d1bcb6
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-04 23:47:44 +0530

    day 17 solution

--- Day 17 More Exceptions/day 17-solution.js
+++ Day 17 More Exceptions/day 17-solution.js
@@ -1,2 +1,16 @@
 // code snippet for Day 17: More Exceptions
 
+
+//Write your code here
+class Calculator{
+    power(n, p) {
+
+        if (n < 0 || p < 0) {
+            throw "n and p should be non-negative"
+        }else {
+            return n**p
+        }
+
+    }
+}
+
