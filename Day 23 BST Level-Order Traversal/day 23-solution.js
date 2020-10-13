40465df18379f8eef0c5b414e999ff845f1a48ac
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-13 20:25:32 +0530

    day 23 solution

--- Day 23 BST Level-Order Traversal/day 23-solution.js
+++ Day 23 BST Level-Order Traversal/day 23-solution.js
@@ -1,2 +1,20 @@
 // code snippet for Day 23: BST Level-Order Traversal
 
+        // Add your code here
+        // To print values separated by spaces use process.stdout.write(someValue + ' ')
+        if (root){
+            let queue = [root];
+
+            while (queue.length) {
+                let tree = queue.shift();
+                process.stdout.write(tree.data + ' ');
+
+                if (tree.left){
+                    queue.push(tree.left);
+                }
+                if (tree.right){
+                    queue.push(tree.right);
+                }
+            }
+            return queue;
+        }   
