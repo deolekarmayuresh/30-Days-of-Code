8dad0cb142a845c886a15c71cf54d01bddcbf8db
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-18 01:32:22 +0530

    day 26 solution

--- Day 26 Nested Logic/day26-solution.js
+++ Day 26 Nested Logic/day26-solution.js
@@ -1,2 +1,33 @@
 // code snippet for Day 26: Nested Logic
 
+function processData(input) {
+    //Enter your code here
+    let actual = input.split("\n")[0].split(" ").map(num => parseInt(num));
+    let expected = input.split("\n")[1].split(" ").map(num => parseInt(num));
+    let [actualDay, actualMonth, actualYear] = actual;
+    let [expectedDay, expectedMonth, expectedYear] = expected;
+
+    if(actualYear > expectedYear){
+        console.log(10000);
+    }
+    else if(actualYear === expectedYear && actualMonth > expectedMonth){
+        console.log((actualMonth - expectedMonth) * 500);
+    }
+    else if(actualMonth === expectedMonth && actualDay > expectedDay){
+        console.log((actualDay - expectedDay) * 15);
+    }
+    else{
+        console.log(0);
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
